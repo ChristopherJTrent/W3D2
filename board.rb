fc03f331f1f3cc5ffd87145ed19d943e81512460
David <euntek95@gmail.com>
2024-01-23 11:29:02 -0800

    created populate method

--- board.rb
+++ board.rb
@@ -3,6 +3,18 @@ require_relative 'card.rb'
 class Board 
 
     def initialize
-        @grid = Array.new(4) { Array.new }
+        @grid = Array.new(4) { Array.new(4) }
+    end
+
+    def populate
+        faces = []
+        ('A'..'H').each do |char|
+            faces << char
+            faces << char 
+        end
+        faces.shuffle!
+        @grid.map! do |inner|
+            inner.map! { |ele| ele = faces.pop }
+        end
     end
 end
