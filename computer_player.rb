c20b73a877d9fe2dbad504623fb199d5eca80639
ChristopherJtrent <ChristopherJtrent@outlook.com>
2024-01-23 16:03:34 -0800

    implement match method

--- computer_player.rb
+++ computer_player.rb
@@ -3,13 +3,30 @@ require_relative 'player.rb'
 class ComputerPlayer < Player
 
     def initialize
-        @known_cards = {}
+        @known_cards = Hash.new{|hash, key| hash[key] = Array.new}
         @matched_cards = []
         @cached_reply = nil 
     end
 
+    def match 
+        # for each key-value pair in known cards
+        #       where key is a string "face"
+        #       and value is an array of [x,y] positions
+        #   if the value's length is greater than 1
+        #       add that value to @matched_cards
+        #       set the value at key to nil
+        @known_cards.each_pair do |key, value|
+            if value.length > 1
+                @matched_cards << value
+                @known_cards[key] = nil
+            end
+        end
+    end
+
     def receive_revealed_card(pos, face)
-        @known_cards[pos] = face
+        # {face => [pos, pos, pos]}
+        @known_cards[face] << pos
+        match
     end
 
     def receive_match(pos1, pos2)
